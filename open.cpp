d1df740934da57b969bcdf630afc98d4dfe2361d
pengpeng <prophet_peng@163.com>
2015-11-08 17:18:42 +0800

    add file open.cpp

---
+++ open.cpp
@@ -0,0 +1,17 @@
+#include <Linux/module.h>
+#include <Linux/kernel.h>
+#include <Linux/init.h>
+#include <Linux/syscalls.h>
+#include <Linux/file.h>
+#include <Linux/fs.h>
+#include <Linux/string.h>
+#include <Linux/mm.h>
+#include <Linux/sched.h>
+#include <Linux/unistd.h>
+#include <net/sock.h>
+#include <net/netlink>
+
+#define SecurityPath "/home/pp"
+#define MAX_LENGTH 256
+
+
